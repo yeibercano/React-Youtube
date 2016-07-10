4702b85278a3b9631ea018c485bf3c3598bf8cb0
Yeiber Cano <yeibercano@gmail.com>
2016-07-10 00:04:19 -0700

    add selected video

--- src/index.js
+++ src/index.js
@@ -13,10 +13,16 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [], 
+			selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'pusherman'}, (videos) => {
-			this.setState({ videos })
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			})
 		})
 	}
 
@@ -25,8 +31,10 @@ class App extends Component {
 			<div>
 		    <h1 className='title'> React - Redux  </h1>
 	      <SearchBar /> 
-	      <VideoDetail video={this.state.videos[0]}/>
-	      <VideoList videos={this.state.videos} /> 
+	      <VideoDetail video={this.state.selectedVideo}/>
+	      <VideoList 
+  				onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+	        videos={this.state.videos} /> 
 		  </div>
 		);  
   }
