abe9adb736aea1c731d6dca9fa36ee8058cc7f35
Yeiber Cano <yeibercano@gmail.com>
2016-07-09 20:40:18 -0700

    add event handler to test input using ES6

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
 
 	render() {
-    return <input />
+    return <input onChange={ event => console.log(event.target.value) } />
 	}
   
 }
