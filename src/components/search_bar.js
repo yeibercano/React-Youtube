1a91edd68abae9931fca2d3ca0ef3a466055b548
Yeiber Cano <yeibercano@gmail.com>
2016-07-09 21:11:32 -0700

    add control input value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,13 @@ class SearchBar extends Component {
   }
 
 	render() {
-    return <input onChange={ event => console.log(event.target.value) } />
+    return ( 
+    	<div>
+	      <input 
+	        value={this.state.term}
+	        onChange={ event => this.setState({ term: event.target.value })} />
+      </div>
+    )
 	}
   
 }
