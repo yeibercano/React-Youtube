dd9f5486c84459b7b480c12568134360b512d003
Yeiber Cano <yeibercano@gmail.com>
2016-07-09 22:15:54 -0700

    add unique keys to each videoItem

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,9 +4,9 @@ import VideoListItem from './video_list_item';
 
 const VideoList = ( props ) => {
 	const videoItems = props.videos.map((video) => {
-		return <VideoListItem video={video} />
+		return <VideoListItem key={video.etag} video={video} />
 	})
-	
+
 	return (
 		<ul className='col-md-4 list-group'>
       {videoItems}
