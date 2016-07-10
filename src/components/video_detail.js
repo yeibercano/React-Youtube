278199501567aa217674a9bd6ab8ae95dd4d1f6b
Yeiber Cano <yeibercano@gmail.com>
2016-07-09 23:45:39 -0700

    add check before it renders to avoid undefined

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
+  if (!video) {
+  	return <div>Loading...</div>
+  }
+
 	const videoId = video.id.videoId;
-	const url = `https://wwww.youtube.com/embed/${videoId}`;
+	const url = `https://www.youtube.com/embed/${videoId}`;
   return (
   	<div className='video-detail col-md-8'>
       <div className='embed-responsive embed-responsive-16by9'>
