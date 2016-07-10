6460444feb0f15c1d8e1377d4c2e5caf41015a98
Yeiber Cano <yeibercano@gmail.com>
2016-07-09 23:44:26 -0700

    inject videoDetail & pass first video as default

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBv20z2sRAEnXu-7oD9zfg5q6FvmM2gPMw';
 
@@ -25,6 +25,7 @@ class App extends Component {
 			<div>
 		    <h1 className='title'> React - Redux  </h1>
 	      <SearchBar /> 
+	      <VideoDetail video={this.state.videos[0]}/>
 	      <VideoList videos={this.state.videos} /> 
 		  </div>
 		);  
