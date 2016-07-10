429e364f1dee2b57e7da0ac2a131b48b18fd9e97
Yeiber Cano <yeibercano@gmail.com>
2016-07-09 20:17:51 -0700

    import and set SearchBar component and key

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBv20z2sRAEnXu-7oD9zfg5q6FvmM2gPMw';
 
 // set new component
 
 const App = () => {
-	return <div className='title'> React - Redux - Webpack - Node - PostCSS </div>;
+	return ( 
+		<div className='title'>
+	    React - Redux - Webpack - Node - PostCSS 
+      <SearchBar /> 
+	  </div>
+	);  
 }
 
 // render component to DOM
