29f3d8fa97c7cbee3a9181fa55ac5b7e1e6ae01b
Yeiber Cano <yeibercano@gmail.com>
2016-07-09 21:40:18 -0700

    refactor to class using ES6 and making API call

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBv20z2sRAEnXu-7oD9zfg5q6FvmM2gPMw';
 
-// set new component
 
-const App = () => {
-	return ( 
-		<div>
-	    <h1 className='title'> React - Redux  </h1>
-      <SearchBar /> 
-	  </div>
-	);  
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'pusherman'}, (videos) => {
+			this.setState({ videos })
+		})
+	}
+
+  render() {
+		return ( 
+			<div>
+		    <h1 className='title'> React - Redux  </h1>
+	      <SearchBar /> 
+		  </div>
+		);  
+  }
 }
 
-// render component to DOM
 ReactDOM.render(<App />, document.querySelector('.container'))
